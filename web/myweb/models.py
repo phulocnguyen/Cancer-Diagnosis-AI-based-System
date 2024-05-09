d8b98e1287fc72a276016d57ea7ca0d3a0d12f40
BNK2610 <117821044+BNK2610@users.noreply.github.com>
2024-05-09 20:44:38 +0000

    update login and register

--- web/myweb/models.py
+++ web/myweb/models.py
@@ -5,6 +5,7 @@ class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     email = models.CharField(max_length=200, null=True)
     password = models.CharField(max_length=200, null=True)
+    full_name = models.CharField(max_length=200, null=True) 
 
     def __str__(self):
-        return self.user
+        return self.full_name if self.full_name else str(self.user)
--- web/myweb/views.py
+++ web/myweb/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from .models import *
 
 # Create your views here.
 def abstract(request):
@@ -17,19 +20,52 @@ def index(request):
     context={}
     return render(request, 'index.html', context)
 
-def login(request):
-    context={}
-    return render(request, 'login.html', context)
-
 def preview(request):
     context={}
     return render(request, 'preview.html', context)
-
-def signup(request):
-    context={}
-    return render(request, 'signup.html', context)
 
 def upload(request):
     context={}
     return render(request, 'upload.html', context)
 
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        full_name = request.POST.get('full_name') 
+        
+        if User.objects.filter(username=username).exists():
+            msg = "Username already exists"
+            return render(request, 'signup.html', {'msg': msg})
+        elif Customer.objects.filter(email=email).exists():
+            msg = "Email already exists"
+            return render(request, 'signup.html', {'msg': msg})
+        else:
+            # Tạo người dùng mới
+            user = User.objects.create_user(username=username, email=email, password=password)
+            
+            # Tạo khách hàng mới và liên kết với người dùng tạo mới
+            customer = Customer.objects.create(user=user, email=email, password=password, full_name=full_name)
+            
+            # Đăng nhập người dùng tự động sau khi đăng ký thành công
+            #user = authenticate(username=username, password=password) # type: ignore
+            #login(request, user)
+            
+            return redirect('/')  # Chuyển hướng đến trang chính sau khi đăng ký thành công
+    else:
+        return render(request, 'signup.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            # login(request, user)  
+            return redirect('/')  # Chuyển hướng đến trang chính sau khi đăng nhập thành công
+        else:
+            msg = "Invalid username or password. Please try again."
+            return render(request, 'login.html', {'msg': msg})
+    else:
+        return render(request, 'login.html')
